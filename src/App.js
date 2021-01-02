49afdb15c001abc3593be3c77205917c38f21831
Arnob Almazee <arnobxtreme@gmail.com>
2021-01-02 23:14:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { PageHeader, Row, Col, Card  } from 'antd';
+import Product from './product.js'
+
+const { Meta } = Card;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <PageHeader
+        title="RND_1"
+      />
+      <Row
+        gutter={16}
+        style={{
+          padding: 10,
+          marginTop: 10
+        }}
+      >
+        {
+          Product.map(ii =>
+            <Col xs={24} sm={24} md={16} lg={4} xl={4}>
+              <Card
+                hoverable
+                style={{ width: 240, margin: 5, minHeight: 600, marginTop: 20 }}
+                cover={<img alt="example" src={ii.img} />}
+              > 
+
+                <b>{ii.name}</b>
+                <Meta style={{ marginTop: 5 }} description={ii.details} />
+
+                <div
+                  style={{
+                    position: 'absolute',
+                    right: 0,
+                    bottom: 0,
+                    width: '100%',
+                    borderTop: '1px solid #e9e9e9',
+                    padding: '10px 16px',
+                    background: '#fff',
+                    textAlign: 'right',
+                  }}
+                >
+                  <Meta title="Price" />
+                  <Meta title={ii.price} />
+                </div>
+              </Card>
+            </Col>
+          )
+        }
+
+        {/* <Col xs={24} sm={24} md={16} lg={8} xl={8}>
+          Col
+        </Col>
+        <Col xs={24} sm={24} md={16} lg={8} xl={8}>
+          Col
+        </Col> */}
+      </Row>
+    </Fragment>
   );
 }
 
